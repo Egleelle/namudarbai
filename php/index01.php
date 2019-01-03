0766e28e9b2088c29a7ef78776ec72da04f157cd
Eglė Karosaitė <eglek@cargoandgo.local>
2019-01-03 09:25:23 +0200

    pirmasphp

---
+++ php/index01.php
@@ -0,0 +1,33 @@
+
+<?php $salis = 'Lietuva';
+      $miestas = 'Vilnius';
+      $populiacija = '574221';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+
+    <div>
+
+    <ul> Šalis</ul>
+        <li> <?php echo $salis; ?></li>
+    <ul> Miestas </ul>
+        <li><?php echo $miestas; ?></li>
+    <ul> Populiacija</ul>
+        <li><?php echo $populiacija; ?></li>
+
+
+
+    </div>
+
+</body>
+</html>
